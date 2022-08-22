49f7df2b1712e567c72e065adfb5372216fb86d5
Ahmed Ashraf <ahmdgun0@gmail.com>
2022-08-22 12:45:21 +0200

    Update index.jsx

--- components/Post/index.jsx
+++ components/Post/index.jsx
@@ -16,7 +16,6 @@ const CAPTION_SIZE = 100;
 
 const Post = ({ data, clientUsername, isVerified }) => {
   const [isLove, setisLove] = useState(false);
-  console.log(data)
 
   const dispatch = useDispatch();
 
